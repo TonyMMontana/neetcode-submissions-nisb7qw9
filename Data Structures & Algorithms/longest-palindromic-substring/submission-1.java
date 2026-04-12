7b647a014902a32e5e368a6381db8ab169b7dc2f
Anton <anton.haiduk.job@gmail.com>
2026-04-12 13:47:49 +0300

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    Map<String, Boolean> cache;
+
+    public String longestPalindrome(String s) {
+        String res = "";
+        cache = new HashMap<>();
+        for(int i = 0; i < s.length(); i++) {
+            for(int r = i; r < s.length(); r++) {
+                if(isPal(i, r, s)) {
+                    if(res.length() < r - i + 1) {
+                        res = s.substring(i, r + 1);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean isPal(int l, int r, String s) {
+        if(l >= r) {
+            return true;
+        }
+        String key = String.valueOf(l) + String.valueOf(r);
+        if(cache.containsKey(key)) {
+            return cache.get(key);
+        }
+        if(s.charAt(l) != s.charAt(r)) {
+            return false;
+        }
+        cache.put(key, isPal(l + 1, r - 1, s));
+        return cache.get(key);
+    }
+}
