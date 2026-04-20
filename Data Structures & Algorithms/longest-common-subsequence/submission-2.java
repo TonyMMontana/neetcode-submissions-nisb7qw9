646043e1d6307978f4b62865512b42ecef246b29
Anton <anton.haiduk.job@gmail.com>
2026-04-20 22:30:19 +0300

    Add: longest-common-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[] dp = new int[text2.length() + 1];
+        dp[text2.length()] = 0;
+
+    //cur == bottom
+    //cur == bottom -> right 
+        
+        for(int i = text1.length() - 1; i >= 0; i--) {
+            int diag = 0;
+            for(int j = text2.length() - 1; j >= 0; j--) {
+                int temp = dp[j];
+                if(text1.charAt(i) == text2.charAt(j)) {
+                    //update diagonal
+                    dp[j] = diag + 1;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j + 1]);
+                }
+                diag = temp;
+            }
+        }
+        return dp[0];
+    }
+}
