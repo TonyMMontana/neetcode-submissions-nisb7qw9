e389dd524608b379051a4e58f7b33ae6638329c5
Anton <anton.haiduk.job@gmail.com>
2026-05-01 01:04:29 +0300

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tank = 0;
+        int diff = 0;
+        int idx = 0;
+
+        for(int i = 0; i < gas.length; i++) {
+            diff = diff + gas[i] - cost[i];
+            if(tank + gas[i] < cost[i]) {
+                tank = 0;
+                idx = i + 1;
+                continue;
+            }
+            
+            tank = tank + gas[i] - cost[i];
+        }
+        return diff >= 0 ? idx : -1;
+    }
+}
