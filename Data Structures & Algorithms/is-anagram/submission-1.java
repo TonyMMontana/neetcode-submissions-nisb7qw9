ac27583c0cbbf4fe9b4507a928e79a1b6931af55
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-08-14 07:58:33 +0200

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+
+        int[] first = new int[26];
+        int[] second = new int[26];
+
+        for(int i = 0; i < s.length(); i++) {
+            first[s.charAt(i) - 'a']++;
+            second[t.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(first[i] != second[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
