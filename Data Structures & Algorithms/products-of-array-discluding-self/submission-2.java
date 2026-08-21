db90cd850991948e8d0770e4d865c5d8ab39db61
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-08-21 18:08:56 +0200

    Add: products-of-array-discluding-self - submission-2

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        res[0] = 1;
+        res[res.length - 1] = 1;
+        
+        res[0] = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            res[i] = res[i - 1] * nums[i  - 1];
+        }
+
+        int postfix = 1;
+
+        for(int i = res.length - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}  
