7c5d373deffee23d820faed226206f0687115472
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-08-30 09:52:36 +0200

    Add: merge-strings-alternately - submission-2

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int maxLength = Math.max(word1.length(), word2.length());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < maxLength; i++) {
+            if(i >= word1.length()) {
+                sb.append(word2, i, word2.length());
+                break;
+            }
+            if(i >= word2.length()) {
+                sb.append(word1, i, word1.length());
+                break;
+            }
+            sb.append(word1.charAt(i)).append(word2.charAt(i));
+        }
+        return sb.toString();
+    }
+}
