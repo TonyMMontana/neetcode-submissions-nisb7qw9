6f8f06bd96aa5e732f5662e2c1dd9d68435999e7
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-08-31 13:25:24 +0200

    Add: merge-sorted-array - submission-2

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx = nums1.length - 1;
+        int i = m - 1;
+        int j = n - 1;
+
+        while(j >= 0) {
+            if(i >= 0 && nums1[i] >= nums2[j]) {
+                nums1[idx] = nums1[i];
+                i--;
+            } else {
+                nums1[idx] = nums2[j];
+                j--;
+            }
+            idx--;
+        }
+    }
+}
