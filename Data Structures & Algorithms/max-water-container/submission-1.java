aabdd5f7048c75ffe069f71756c3d535e31b1acc
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-09-03 14:30:03 +0200

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int l = 0;
+        int r = heights.length - 1;
+        int maxWater = 0;
+
+
+        while(l < r) {
+            int curWater = Math.min(heights[l], heights[r]) * (r -l);
+            maxWater = Math.max(curWater, maxWater);
+            if(heights[l] <= heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+        return maxWater;
+    }
+}
