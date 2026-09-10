5b16ed9e4cbc466f938c65bdec69faeecddcf481
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-09-10 11:22:12 +0200

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int l = 0;
+        int r = 0;
+        int max = 0;
+
+        while(r < prices.length) {
+            if(prices[r] < prices[l]) {
+                l = r;
+            }
+            max = Math.max(max, prices[r] - prices[l]);
+            r++;
+        }
+        return max;
+    }
+}
