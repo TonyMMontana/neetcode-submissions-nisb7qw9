dec6f9d2f7a11b72032aaa5cbd6da99decce5995
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-09-21 09:08:42 +0200

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            if(cur == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if(cur == ']') {
+                if(stack.isEmpty() ||  stack.pop() != '[') {
+                    return false;
+                }
+            } else if(cur == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else {
+                stack.push(cur);
+            }
+        }
+
+        return stack.size() == 0;
+    }
+}
