65058da25ed9b142a4bcfaa1bcc648f7b4b1f54f
Anton Haiduk <anton.haiduk.job@gmail.com>
2026-09-25 11:00:44 +0200

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+        for(int a : asteroids) {
+            while(!stack.isEmpty() && stack.peek() > 0 && a < 0) {
+                int diff = stack.peek() + a;
+                if(diff < 0) {
+                    stack.pop();
+                } else if(diff > 0) {
+                    a = 0;
+                } else {
+                    a = 0;
+                    stack.pop();
+                }
+            }
+
+            if(a != 0) {
+                stack.push(a);
+            }
+        }
+        int[] res = new int[stack.size()];
+        for(int i = res.length - 1; i >= 0; i--) {
+            res[i] = stack.pop();
+        }
+
+        return res;
+    }
+}
